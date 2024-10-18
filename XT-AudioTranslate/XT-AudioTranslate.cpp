674630cced727bee3d64ed043f6a574c03e3791c
hilderonny <ronny.hildebrandt@avorium.de>
2024-10-18 12:27:02 +0200

    API URL read out from config file

--- XT-AudioTranslate/XT-AudioTranslate.cpp
+++ XT-AudioTranslate/XT-AudioTranslate.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "X-Tension.h"
+#include <cstdio>
 
 #define MAX_MSG_LEN 128
 
 wchar_t buf[MAX_MSG_LEN];
 static const wchar_t* XT_NAME = L"[XT_AudioTranslate]"; // Prefix for messages
+
+char API_URL[256];
 
 // http://www.x-ways.net/forensics/x-tensions/XWF_functions.html#E
 
@@ -32,8 +35,24 @@ LONG __stdcall XT_About(HANDLE hParentWnd, void* lpReserved)
 
 LONG __stdcall XT_Prepare(HANDLE hVolume, HANDLE hEvidence, DWORD nOpType, void* lpReserved)
 {
-	swprintf(buf, MAX_MSG_LEN, L"%ls XT_Prepare", XT_NAME);
-	XWF_OutputMessage(buf, 0);
+	FILE* file = nullptr; // Dateizeiger initialisieren
+	errno_t err = fopen_s(&file, "XT-AudioTranslate.cfg", "r"); // Datei im Lese-Modus ˆffnen
+
+	if (err != 0 || file == nullptr) { // ‹berpr¸fen, ob die Datei geˆffnet werden konnte
+		swprintf(buf, MAX_MSG_LEN, L"%ls XT_Prepare : Fehler beim Lesen von XT-AudioTranslate.cfg", XT_NAME);
+		XWF_OutputMessage(buf, 0);
+		return 1;
+	}
+
+	if (fgets(API_URL, sizeof(API_URL), file) != nullptr) { // Eine Zeile lesen
+		size_t len = strlen(API_URL);
+		if (len > 0 && API_URL[len - 1] == '\n') {
+			API_URL[len - 1] = '\0'; // Ersetzen durch Nullterminator
+		}
+		swprintf(buf, MAX_MSG_LEN, L"%ls XT_Prepare : API-URL: %hs", XT_NAME, API_URL);
+		XWF_OutputMessage(buf, 0);
+	}
+	fclose(file); // Datei schlieﬂen
 	return 0;
 }
 
