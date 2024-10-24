070f0c355400cc94017a7cab4f5f4029b4f9f815
hilderonny <ronny.hildebrandt@avorium.de>
2024-10-24 10:56:49 +0200

    Code cleanup

--- XT-AudioTranslate/TaskBridge.h
+++ XT-AudioTranslate/TaskBridge.h
@@ -3,10 +3,6 @@
 
 void sendFile(const char* server, const char* filePath, const char* endpoint);
 
-void getrequest();
-
 std::string httpGET(const std::string& url);
-
-std::string httpGET(const std::string& server, const std::string& path);
 
 std::string translate();
